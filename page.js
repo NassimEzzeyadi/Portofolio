436381966b69889aa24683b37d9bc8b541b824bc
Nassim Ezzeyadi <nassimezzeyadi@gmail.com>
2023-02-22 16:49:18 +0100

    Portofolio

---
+++ page.js
@@ -0,0 +1,7 @@
+ // Add your JavaScript here
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', function() {
+document.body.classList.toggle('dark-mode');
+});
+    
